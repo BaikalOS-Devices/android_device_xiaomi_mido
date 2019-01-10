7b1ccee232aa953b6e00fe63c1bd3671a1a2d0d0
Bruno Martins <bgcngm@gmail.com>
2019-01-10 11:51:54 +0100

    mido: Convert XiaomiDoze into XiaomiParts
     * This is the preparation for turning the package into
       an unified device-settings app.
    
    Change-Id: I255f1ab8dab0e1037dee766a5ba5ee9f62fdfe6d
    Signed-off-by: sayan7848 <sayan7848@gmail.com>

--- device.mk
+++ device.mk
@@ -149,9 +149,9 @@ PRODUCT_PACKAGES += \
     libgenlock \
     libtinyxml
 
-# Doze mode
+# Device-specific settings
 PRODUCT_PACKAGES += \
-    XiaomiDoze
+    XiaomiParts
 
 # DRM
 PRODUCT_PACKAGES += \
